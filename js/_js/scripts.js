33b93117f9ef81a9e01681a4e8e96dfcd9a10baf
pavelp21 <pavelvp2182@yandex.ru>
2019-08-12 20:31:17 +0500

    Revs and prices edits

--- js/_js/scripts.js
+++ js/_js/scripts.js
@@ -1,9 +1,22 @@
 //@prepros-prepend jquery-2.1.1.min.js
-//@prepros-prepend bootstrap.min.js
+
 
 $(document).ready(function () {
+    /* Якорь */
+    $(function () {
+        $("a[href^='#prices']").click(function (h) {
+            h.preventDefault();
+            var f = $(this).attr("href"),
+                g = $(f).offset().top;
+            $("body,html").animate({
+                scrollTop: g
+            }, 1500)
+        });
+    });
 
-
-
+    $(".video-wrapper-revs img").click(function () {
+        var a = $(this).parent().attr("data-youtube");
+        $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1&mute=1"  allowfullscreen></iframe>')
+    });
     /*Конец документа*/
 });
