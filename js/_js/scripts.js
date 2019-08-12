b6411564713b8f4a149b4dc6b42646bd91f075c2
pavelp21 <pavelvp2182@yandex.ru>
2019-08-12 11:46:11 +0500

    Header is done

--- js/_js/scripts.js
+++ js/_js/scripts.js
@@ -1,8 +1,9 @@
 //@prepros-prepend jquery-2.1.1.min.js
+//@prepros-prepend bootstrap.min.js
 
-$(document).ready(function() {
+$(document).ready(function () {
 
 
 
-/*Конец документа*/
+    /*Конец документа*/
 });
